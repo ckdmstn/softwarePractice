5bb9c30d799ca79b3ba8c1cf962653301d33991c
ckdmstn <eunsu321@naver.com>
2023-05-31 18:33:48 +0900

    [CHORE] Validator 수정

--- src/main/java/com/example/softwarePractice/service/AccountFormValidator.java
+++ src/main/java/com/example/softwarePractice/service/AccountFormValidator.java
@@ -19,7 +19,6 @@ public class AccountFormValidator implements Validator {
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "id", "ID_REQUIRED", "아이디는 필수 항목입니다.");
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "userName", "USER_NAME_REQUIRED", "이름은 필수 항목입니다.");
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "nickName", "NICK_NAME_REQUIRED", "닉네임은 필수 항목입니다.");
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "PASSWORD_REQUIRED", "비밀번호는 필수 항목입니다.");
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "email", "EMAIL_REQUIRED", "이메일은 필수 항목입니다.");
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "phoneNumber", "PHONE_REQUIRED", "전화번호는 필수 항목입니다.");
 
@@ -33,8 +32,10 @@ public class AccountFormValidator implements Validator {
                 errors.rejectValue("zipcode", "ZIPCODE_NOT_CORRECT", "잘못된 우편번호 형식입니다. ex)12345");
         }
 
-        if (!memReq.getPassword().equals(memReq.getPasswordCheck())) { // 비번, 비번 확인이 다르면
-            errors.rejectValue("passwordCheck", "PW_CHECK_DIFF", "비밀번호가 다릅니다.");
+        if (memReq.getPassword() != null && memReq.getPassword().length() > 0) {
+            if (!memReq.getPassword().equals(memReq.getPasswordCheck())) { // 비번, 비번 확인이 다르면
+                errors.rejectValue("passwordCheck", "PW_CHECK_DIFF", "패스워드가 일치하지 않습니다.");
+            }
         }
 
         // 이메일 형식 확인 (추후)
