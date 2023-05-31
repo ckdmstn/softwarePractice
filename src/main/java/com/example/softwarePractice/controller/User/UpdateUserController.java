2589ea4590209d6f65d9062d764ffeb60e3a2f9d
ckdmstn <eunsu321@naver.com>
2023-05-31 17:07:45 +0900

    [ADD/FEAT] myPage 관련 컨트롤러 및 서비스 구현

---
+++ src/main/java/com/example/softwarePractice/controller/User/UpdateUserController.java
@@ -0,0 +1,131 @@
+package com.example.softwarePractice.controller.User;
+
+import com.example.softwarePractice.domain.Account;
+import com.example.softwarePractice.service.AccountFormValidator;
+import com.example.softwarePractice.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.WebUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/user/myPage")
+@SessionAttributes("userSession")
+public class UpdateUserController {
+    private static final String USER_MYPAGE_FORM = "user/myPageUpdateForm";
+    private static final String USER_MYPAGE = "user/myPage";
+
+    @Autowired
+    private AccountService accountService;
+    public void setAccountService(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @Autowired
+    private AccountFormValidator validator;
+    public void setValidator(AccountFormValidator validator) {
+        this.validator = validator;
+    }
+
+    @ModelAttribute("accountReq")
+    public UserRegistRequest formBacking(HttpServletRequest request) {
+        if (request.getMethod().equalsIgnoreCase("GET")) {
+            UserSession userSession = (UserSession) WebUtils.getSessionAttribute(request, "userSession");
+            Account account = userSession.getAccount();
+
+            UserRegistRequest accountReq = new UserRegistRequest(
+                    account.getUserName(), account.getNickName(), account.getId(),
+                    account.getEmail(), account.getAddress(), account.getZipcode(),
+                    account.getBankName(), account.getBankAccount(), account.getPhone()
+            );
+
+            return accountReq;
+        }
+        return new UserRegistRequest();
+    }
+
+    @ModelAttribute("myPageList")
+    public int[] getMyPageList(HttpServletRequest request) { // 구매내역, 판매내역, 공동구매내역, 위시리스트 개수 반환
+        if (request.getMethod().equalsIgnoreCase("GET")) {
+            UserSession userSession = (UserSession) WebUtils.getSessionAttribute(request, "userSession");
+            Account account = userSession.getAccount();
+
+            int[] myPageList = accountService.getMyPageList(account.getId());
+
+            return myPageList;
+        }
+        return null;
+    }
+
+    @GetMapping
+    public String show() {
+        return USER_MYPAGE;
+    }
+
+    @GetMapping("/update")
+    public String showForm() {
+        return USER_MYPAGE_FORM;
+    }
+
+    // 닉네임, 전화번호, 이메일, 주소, 은행명, 계좌번호만 변경 가능
+    @PostMapping("/update")
+    public String update(HttpServletRequest request,
+                         @Valid @ModelAttribute("accountReq") UserRegistRequest accountReq,
+                         BindingResult bindingResult) {
+        validator.validate(accountReq, bindingResult);
+
+        UserSession userSession = (UserSession) WebUtils.getSessionAttribute(request, "userSession");
+
+        Account account = userSession.getAccount();
+        account.setNickName(accountReq.getNickName());
+        account.setPhone(accountReq.getPhoneNumber());
+        account.setEmail(accountReq.getEmail());
+        account.setAddress(accountReq.getAddress());
+        account.setBankName(accountReq.getBankName());
+        account.setBankAccount(accountReq.getBankAccount());
+
+        Account newAccount = accountService.updateAccount(account);
+        userSession.setAccount(newAccount);
+
+        return "redirect:/" + "user/myPage";
+    }
+
+    @PostMapping("/update/password")
+    public String updatePassword( HttpServletRequest request,
+                                  @RequestParam("currentPassword") String currentPassword,
+                                  @RequestParam("newPassword") String newPassword,
+                                  @RequestParam("newPasswordCheck") String newPasswordCheck) {
+
+        UserSession userSession = (UserSession) WebUtils.getSessionAttribute(request, "userSession");
+        Account account = userSession.getAccount();
+
+        if (account.getPassword().equals(currentPassword)) { // 현재 비밀번호 틀릴 경우
+            return "redirect:/" + "user/myPage"; // 추후 수정
+        }
+
+        if (newPassword.equals(newPasswordCheck)) { // 새 비밀번호와 비밀번호 확인이 다를 경우
+            return "redirect:/" + "user/myPage"; // 추후 수정
+        }
+
+        accountService.updatePassword(account.getId(), newPassword);
+
+        return "redirect:/" + "user/myPage";
+    }
+
+    @PostMapping("/delete")
+    public String delete(HttpServletRequest request) {
+        UserSession userSession = (UserSession) WebUtils.getSessionAttribute(request, "userSession");
+        Account account = userSession.getAccount();
+        accountService.deleteAccount(account);
+
+        return "redirect:/" + "/main";
+    }
+
+    private static boolean isLoggedIn(UserSession userSession) {
+        return (userSession != null);
+    }
+}
--- src/main/java/com/example/softwarePractice/service/AccountService.java
+++ src/main/java/com/example/softwarePractice/service/AccountService.java
@@ -60,8 +60,8 @@ public class AccountService {
     }
 
     // 회원 정보 수정 후 다시 Account 반환
-    public void updateAccount(Account account) {
-        accountDao.updateAccount(account);
+    public Account updateAccount(Account account) {
+        return accountDao.updateAccount(account);
     }
 
     // 회원 정보 비밀번호 변경
@@ -72,5 +72,55 @@ public class AccountService {
     // 회원 삭제
     public void deleteAccount(Account account) {
         accountDao.deleteAccount(account);
+    }
+
+    // 사용자 PK로 구매 내역, 판매 내역, 공동구매 내역, 위시리스트 개수 반환
+    public int[] getMyPageList(String id) {
+        return new int[] {1, 0, 0, 2}; // 추후 수정!!
+    }
+
+    // 사용자 PK로 판매 내역 리스트 검색
+//    public List<PersonalItem> getSellItemList(String id) {
+//        return null;
+//    }
+
+    // 사용자 PK로 공동구매 판매 내역 리스트 검색
+//    public List<GroupItem> getSellGroupList(String id) {
+//        return null;
+//    }
+
+    // 사용자 PK로 구매 내역 리스트 검색
+//    public List<Order> getOrderList(String id) {
+//        return null;
+//    }
+
+    // 구매 취소
+    public void cancelOrder(String id, int orderId) {
+
+    }
+
+    // 사용자 PK로 개인 판매 위시리스트 검색
+//    public List<PersonalItem> getPersonalWishlist(String id) {
+//        return null;
+//    }
+
+    // 사용자 PK로 공동구매 위시리스트 검색
+//    public List<GroupItem> getGroupWishlist(String id) {
+//        return null;
+//    }
+
+    // 사용자 PK로 학교 굿즈 위시리스트 검색
+//    public List<SomsomItem> getSomsomWishlist(String id) {
+//        return null;
+//    }
+
+    // 해당 사용자에 해당 아이템 위시리스트 추가
+    public void addWishlist(String id, int itemId) {
+
+    }
+
+    // 해당 사용자에 해당 아이템 위시리스트 삭제
+    public void cancelWishlist(String id, int itemId) {
+
     }
 }
