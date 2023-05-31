2b13340410d29805386ed0b27436efab087c0747
ckdmstn <eunsu321@naver.com>
2023-05-31 15:46:48 +0900

    [CHORE] 자동로그인 추가

--- src/main/java/com/example/softwarePractice/controller/User/RegisterUserController.java
+++ src/main/java/com/example/softwarePractice/controller/User/RegisterUserController.java
@@ -5,6 +5,7 @@ import com.example.softwarePractice.service.AccountService;
 import com.example.softwarePractice.service.AccountFormValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.WebUtils;
@@ -44,10 +45,8 @@ public class RegisterUserController {
                     account.getUserName(), account.getNickName(), account.getId(),
                     account.getEmail(), account.getPhone()
             );
-            System.out.println("formBacking, if");
             return memReq;
         } else {
-            System.out.println("formBacking, else");
             return new UserRegistRequest();
         }
     }
@@ -58,29 +57,25 @@ public class RegisterUserController {
 
     @GetMapping
     public String showForm() {
-        System.out.println("showForm");
         return USER_REGISTRATION_FORM;
     }
 
     @PostMapping
     public String submit(HttpSession session,
                          @Valid @ModelAttribute("memReq") UserRegistRequest memReq,
-                         BindingResult bindingResult) {
+                         BindingResult bindingResult, Model model) {
         validator.validate(memReq, bindingResult);
-
 
         if (bindingResult.hasErrors()) {
             return USER_REGISTRATION_FORM;
         }
 
-
         Account account = accountService.insertAccount(memReq); // 계정 생성
         UserSession userSession = new UserSession(account); // account를 세션에 저장
         session.setAttribute("userSession", userSession);
+        model.addAttribute("userSession", userSession);
         // -> 따로 로그인하지 않아도 회원가입 완료하면 자동 로그인
 
-        System.out.println("1 " + account.toString());
-        System.out.println("2 " + userSession.getAccount().toString());
 
         return "index"; // 메인화면으로 redirection
     }
